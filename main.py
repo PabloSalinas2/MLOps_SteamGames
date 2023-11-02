e96f5cd19a406f6ae0c7e93c3ee284caca0e6582
PabloSalinas2 <gesalinas.b@gmail.com>
2023-11-02 02:38:14 -0300

    colocar funciones en sus respectivas rutas

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ df_funcion5=pd.read_csv('funcion5.csv')
 
 app= FastAPI()
 
-@app.get("/")
+@app.get("/play_time_genre/{genre}")
 def PlayTimeGenre( genre : str ): #  Debe devolver año con mas horas jugadas para dicho género.
     # pasar a dataframe dentro de la funcion ?
 
@@ -24,7 +24,7 @@ def PlayTimeGenre( genre : str ): #  Debe devolver año con mas horas jugadas pa
         print('Genero incorrecto')
 
 
-
+@app.get("/user_for_genre/{genero}")
 def UserForGenre( genero : str ): 
     try:
         genero=genero.capitalize() 
@@ -48,7 +48,7 @@ def UserForGenre( genero : str ):
 
 
 
-
+@app.get("/users_recommend/{año}")
 def UsersRecommend( año : int ):
     try:
         df_top3=df_funcion3[df_funcion3['año_posted_review']==año].nlargest(3,'recommend')
@@ -60,7 +60,7 @@ def UsersRecommend( año : int ):
 
 
 
-
+@app.get("/users_not_recommend/{año}")
 def UsersNotRecommend( año : int ): # Devuelve el top 3 de juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
     try:
         df_top3=df_funcion3[df_funcion3['año_posted_review']==año].nsmallest(3,'recommend')
@@ -73,7 +73,7 @@ def UsersNotRecommend( año : int ): # Devuelve el top 3 de juegos MENOS recomen
 
 
 
-
+@app.get("/sentiment_analysis/{año}")
 def sentiment_analysis( año : int ): 
     df_año=df_funcion5[df_funcion5['año_lanzamiento']==año]
     positivos=0
